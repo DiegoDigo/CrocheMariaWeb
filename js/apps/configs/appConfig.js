e7a4ed80da4496c9b9db55f2172cd706f4ea1ad2
Diego <di3g0d0ming05@gmail.com>
2016-11-28 16:20:06 -0200

    rotas

--- js/apps/configs/appConfig.js
+++ js/apps/configs/appConfig.js
@@ -1,10 +1,16 @@
 angular.module("crocheMaria").config(function($routeProvider,$locationProvider){
-      $locationProvider.html5Mode(true);
       $routeProvider
-
       .when('/',{
-          url: '/home',
           templateUrl : 'views/home.html',
           controller : 'homeController',
+          controllerAs :'home',
       })
+
+      .when('/produtos',{
+          templateUrl : 'views/produtos.html',
+          controller : 'produtoController',
+          controllerAs : 'produto',
+      });
+
+      // $locationProvider.html5Mode(true);
 });
---
+++ js/controllers/produtos/produtoController.js
@@ -0,0 +1,4 @@
+angular.module("crocheMaria").controller("produtoController", function(){
+    var vim = this;
+    vim.testes = [1,2,3,4,5,6,7,8,9];
+});
